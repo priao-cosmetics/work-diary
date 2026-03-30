18dfe3d7919a72a4941849c20e73d903fa8ad89c
FlukeSuthipong <fx.239suthipong@gmail.com>
2026-03-30 14:12:48 +0700

    Update V2.3.3 - force SW refresh new

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 //  Cache Strategy: Cache-First + Version Update
 // ══════════════════════════════════════════════════════
 
-const CACHE_VERSION = 'wd-v2.3.3-2603300643';
+const CACHE_VERSION = 'wd-v2.3.3-2603300656';
 const CACHE_FILES = [
   './',
   './index.html',
